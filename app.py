9d8b4f86e92e8a46ff72523b4a4437be74c3da09
Davis Page <davispage13@gmail.com>
2020-10-30 18:33:23 -0400

    moving files around for heroku

--- app.py
+++ app.py
@@ -14,9 +14,20 @@ app = Flask(__name__)
 
 
 # create route that renders index.html template
-@app.route("/")
+@app.route('/')
 def home():
     return render_template("index.html")
+@app.route("/mL")
+def mL():
+    return render_template("mL.html")
+
+@app.route("/Tableau")
+def Tabluea():
+    return render_template("Tableau.html")
+
+@app.route("/data")
+def data():
+    return render_template("data.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
